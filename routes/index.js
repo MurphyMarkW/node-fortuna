733609fc33582c53fa9040da72f1190d899d017d
MurphyMarkW <murphy.mark.w+github@gmail.com>
2014-10-09 14:39:31 -0500

    Added in fortune calls.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
+var exec = require('child_process').exec;
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Fortuna - Your daily coffee fortunes!', fortune: 'FORTUNE GO HERE' });
+  exec('fortune -o -s', function(error, stdout, stderr) {
+    res.render('index', { title: 'Fortuna - Your daily coffee fortunes!', fortune: stdout });
+  });
 });
 
 module.exports = router;
